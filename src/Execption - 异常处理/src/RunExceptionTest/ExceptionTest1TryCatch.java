e3c4a15a52a462f10c41fd9ad1fa08264843a4d5
Mrzhi <zhilx1997@sina.com>
2022-01-15 11:08:22 +0800

    v3.3 更新了try-catch异常处理，并的编译时异常进行了try-catch，使得编译不再报错

--- src/Execption - 异常处理/src/RunExceptionTest/ExceptionTest1TryCatch.java
+++ src/Execption - 异常处理/src/RunExceptionTest/ExceptionTest1TryCatch.java
@@ -1,5 +1,10 @@
 package RunExceptionTest;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
  * @Description: 异常处理过程一：try-catch-finally
  * @author : mr.zhi(zhilx1997@sina.com)
@@ -24,7 +29,26 @@ package RunExceptionTest;
  *        ```
  *        说明：
  *        1. finally是可选的
+ * 
  *        2.
+ *        使用try将可能出现异常代码包装起来，在执行过程中，一旦出现异常，就会生成一个对应异常类的对象，根据此对象的类型，去catch中进行匹配。
+ * 
+ *        3.
+ *        一旦try中的异常对象匹配到某一个catch时，就进入catch中进行异常的处理。**一旦处理完成后，会跳出当前的try-catch结构**（在没有写final的情况下），然后继续执行剩下的代码。
+ * 
+ *        4. catch中的异常类型如果没有子父类关系，则谁声明在上，谁声明在下无所谓。
+ *        catch中的异常类型如果满足子父类要求，则要求子类一定声明在父类的上面。否则会报错。**
+ * 
+ *        5. 常用的异常对象处理的方法：
+ *        - 1. String getMessage();
+ *        - 2. printStackTrace(); // 打印堆栈信息
+ * 
+ *        6. 在try结构中声明的变量，当出了try结构追偿吧，就不可以再调用了。
+ *        可以将变量声明在try外面，但是在try的语句块中赋值。
+ * 
+ *        体会：使用try-catch-finally处理编译时异常，使得程序在编译时不再报错，但是运行时仍可能报错。相当于我们使用try-catch-finally将一个编译时可能出现的异常，延迟到运行时出现。
+ * 
+ * 
  */
 public class ExceptionTest1TryCatch {
     public static void main(String[] args) {
@@ -37,7 +61,10 @@ public class ExceptionTest1TryCatch {
                 System.out.println("str转换成功，num = " + num);
 
             } catch (NumberFormatException e) {
-                System.out.println(e);
+                // 在catch中的常用方法 1. String getMessage() 2. printStackTrace()
+                // System.out.println(e);
+                // System.out.println(e.getMessage());
+                e.printStackTrace();
                 System.out.println("数值转换失败，请检查输入是否正确！");
             } catch (NullPointerException e) {
                 System.out.println("出现空指针异常，请检查变量初始化！");
@@ -46,6 +73,28 @@ public class ExceptionTest1TryCatch {
             }
 
             System.out.println("str = " + str);
+        }
+
+        // 2. 对编译异常进行try-catch处理
+        {
+            // 将编译时异常进行try-catch处理，此时可以使得编译不再进行报错
+            try {
+                File file = new File("hello.txt");
+                FileInputStream fis = new FileInputStream("file"); //
+
+                // throws FileNotFoundExceptio
+                int data = fis.read();
+                while (data != -1) {
+                    System.out.println((char) data);
+                    data = fis.read();
+                }
+
+                fis.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
