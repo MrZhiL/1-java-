bd5b80d9d49bc93e83efe9ba4a30d9d094c6cc3f
Mrzhi <zhilx1997@sina.com>
2022-01-16 09:45:41 +0800

    v3.14 更新UID标识，介绍了手动异常类的创建，并在ExceptionThrowTest中进行了测试

--- src/Execption - 异常处理/src/RunExceptionTest/MyExceptionClass.java
+++ src/Execption - 异常处理/src/RunExceptionTest/MyExceptionClass.java
@@ -14,7 +14,7 @@ package RunExceptionTest;
 // 1. 需要继承与现有的异常结构：RuntimeException、Exception
 public class MyExceptionClass extends RuntimeException {
     // 2. 提供一个全局常量 `serialVersionUID`，每个异常类有一个唯一的并且不同的UID。
-    static final long serialVersionUID = -7034897190745766939L;
+    static final long serialVersionUID = -7034812190745766939L;
 
     // 3. 提供重载的构造器
     public MyExceptionClass() {
